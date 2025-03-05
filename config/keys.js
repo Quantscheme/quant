f8e093df381c4491b5b5dba8e47e99ac5420cd50
Your Name <you@example.com>
2025-03-05 05:02:16 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword33 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://quantscheme:QuantumPass@cluster0.ijkwa.mongodb.net/QuantSchemeUsers'
+module.exports = {
+    mongoURI: dbPassword
+};
